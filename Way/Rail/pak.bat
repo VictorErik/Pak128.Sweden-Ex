63820cb0fe9489b06973a234b775e10b2e63bf03
VictorErik <violinvictor@hotmail.com>
2016-01-10 20:50:19 +0100

    Add: Littera Ma

--- Way/Rail/pak.bat
+++ Way/Rail/pak.bat
@@ -1,9 +1,5 @@
 
-..\..\..\..\makeobj PAK128
-..\..\..\..\makeobj PAK128 >error.txt
+..\..\makeobj PAK128
+..\..\makeobj PAK128 >error.txt
 
-Copy *.pak ..\..\..\*.pak
-del *.pak
-
-START ..\..\..\..\Simutrans-Experimental.EXE
 
